8f9adbfee08a4f049bb57d0398b459139dfb36c1
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2018-04-03 16:37:40 +0300

    kurssien listaus ja luonti

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# finalprojectrest
+# REST API DOKUMENTAATIO
+
+<b>RESTIN KÄYTTÖMAHDOLLISUUDET</b>
+
+******************* TICKETS *******************
+
+GET
+/api/tickets
+* listaa kaikki tiketit
+
+/api/tickets/{courseId}
+* listaa kaikki tiketit valitulla kurssiID:llä
+
+POST
+/api/tickets/createticket
+* uuden tiketin luonti (title, description, location)
+* käyttäjä ja kurssi pitää tulla lomakkeelta Reactista (miten??)
+
+PUT
+/api/setpassive/{ticketId}
+* asettaa tiketin passiiviseksi. Eli tämä toiminto kun tiketti "poistetaan" / kun on ratkaistu
+
+******************* USERS *******************
+
+GET
+/api/users
+* listaa kaikki tiketit
+
+POST
+/api/users/createuser
+* uuden käyttäjän luonti (userName)
+* mten yhdistetään käyttäjän luontiin??
+
+*******************COURSES *******************
+
+GET
+/api/courses
+* listaa kaikki kurssit
+
+POST
+/api/courses/createcourse
+* uuden kurssin luonti (courseName)
+
+
+
+<b> OMINAISUUDET TULOSSA APIIN..</b>
+
+OK tikettien listaus (kaikki, kurssi id:n perusteella)
+OK tiketin lisäys
+OK tiketin siirto pois aktiivisesta > muutos passiviksi PUT (poisto)
+
+(* tiketin muokkaus)
+* käyttäjän lisäys/muokkaus/poisto
+* kurssin lisäys/muokkaus/poisto
+
+
+<b>MIETITÄÄN NÄITÄ: </b>
+* HASH / käyttöoikeudet API:iin
+* Käyttäjän luominen MySQL:ään
+* Websocket vs. REST pollaus
+
+<b>LUEMUT REST käyttöön:</b>
+MySQL: luo tietokanta komennolla: CREATE database ticketservice
+IntelJ: vaihda resources > applicatio.properties > oman MySQL:n username & password + muista git IGNORE
---
+++ src/main/java/fi/academy/rest/Controller/CourseController.java
@@ -0,0 +1,50 @@
+package fi.academy.rest.Controller;
+
+import fi.academy.rest.Entity.Course;
+import fi.academy.rest.Entity.Ticket;
+import fi.academy.rest.Repository.CourseRepository;
+import fi.academy.rest.Repository.TicketRepository;
+import fi.academy.rest.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/api")
+public class CourseController {
+
+    private CourseRepository courseRepository;
+    private UserRepository userRepository;
+    private TicketRepository ticketRepository;
+
+    public CourseController(@Autowired CourseRepository courseRepository, @Autowired UserRepository userRepository, @Autowired TicketRepository ticketRepository) {
+        this.courseRepository = courseRepository;
+        this.userRepository = userRepository;
+        this.ticketRepository = ticketRepository;
+    }
+
+    // LIST ALL COURSES
+    @GetMapping("/courses")
+    public Iterable<?> getCourses() {
+        return courseRepository.findAll();
+    }
+
+    // CREATE NEW COURSE
+    @PostMapping("/courses/createcourse")
+    public ResponseEntity createTicket(@RequestBody Course course) throws URISyntaxException {
+        courseRepository.save(course);
+        URI location = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("localhost")
+                .port(8080)
+                .path("api/courses/{id}")
+                .buildAndExpand(course.getCourseId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+}
